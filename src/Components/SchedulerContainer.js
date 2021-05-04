e81b5050e0202bb564866190f3fbc06aa83bc18f
casdidier <cheung.didier@gmail.com>
2021-05-04 21:57:45 +0400

    added switch button for places choices

--- src/Components/SchedulerContainer.js
+++ src/Components/SchedulerContainer.js
@@ -2,6 +2,8 @@ import React, { useState, useContext, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TimeContainer from "./TimeContainer";
 import Button from "@material-ui/core/Button";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -38,6 +40,7 @@ export default function SchedulerContainer() {
   const [timeContainerList, setTimeContainerList] = useState([WEEKDAYS]);
   const [availableDays, setAvailableDays] = useState(WEEKDAYS);
   const [timeFrames, setTimesFrames] = useState([]);
+  const [uniquePlace, setUniquePlace] = useState(true);
 
   const addTimeContainer = () => {
     const newTimeContainerList = [...timeContainerList, availableDays];
@@ -95,6 +98,16 @@ export default function SchedulerContainer() {
 
   return (
     <div className={classes.container}>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={uniquePlace}
+            onChange={() => setUniquePlace(!uniquePlace)}
+            name="Unique Place"
+          />
+        }
+        label={uniquePlace ? "Emplacement unique" : "Plusieurs emplacements"}
+      />
       {timeContainerList.length > 0 &&
         timeContainerList.map((days, index) => {
           return (
